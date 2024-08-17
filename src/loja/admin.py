536c2a8ee2a2732c6a0bd7843a8f8f95185d67b6
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-08-17 16:57:59 -0300

    Senha criptografada

--- src/loja/admin.py
+++ src/loja/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('Nome', 'Email', 'CPF', 'Telefone_celular', 'Telefone_fixo', 'Dt_nasc', 'Endereco', 'Estado', 'CEP')
+    empty_value_display = 'Vazio'
+
+admin.site.register(Cliente, ClienteAdmin)
--- src/loja/models/Cliente.py
+++ src/loja/models/Cliente.py
@@ -1,17 +1,24 @@
+from django.contrib.auth.hashers import make_password
+
 from loja.models import *
 import datetime
 
 class Cliente(models.Model):
     Nome = models.CharField(null=False, max_length=100)
-    email = models.CharField(null=False, max_length=100)
-    CPF = models.CharField(null=False, max_length=11)
+    Email = models.CharField(null=False, max_length=100)
+    CPF = models.CharField(null=False, max_length=11, unique=True)
     Telefone_celular = models.CharField(null=False, max_length=11)
     Telefone_fixo = models.CharField(null=True, max_length=11, blank=True)
     Dt_nasc = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Endereco = models.CharField(null=False, max_length=100)
     Estado = models.CharField(null=False, max_length=100)
     CEP = models.CharField(null=False, max_length=8)
-    senha = models.CharField(null=False, max_length=100)
+    Senha = models.CharField(null=False, max_length=100)
+
+    def save(self, *args, **kwargs):
+        if self.pk is None or self._state.adding:
+            self.Senha = make_password(self.Senha)
+        super(Cliente, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.Nome}'
