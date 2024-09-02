6c535312b9eaebaad58dda386ea684ca186a4ce0
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-09-02 20:11:29 -0300

    Adicionando atributo de "imagem" no modelo Produto

--- src/loja/models/Produto.py
+++ src/loja/models/Produto.py
@@ -9,6 +9,7 @@ class Produto(models.Model):
     Descricao = models.CharField(null=False, max_length=300)
     Dt_fabricacao = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Dt_validade = models.DateField(null=False, default=datetime.date(2000, 1, 1))
+    Imagem = models.ImageField(null=True, blank=True, upload_to='produtos/')
 
     def __str__(self):
         return f'{self.Nome}'
--- src/lojaAdmin/urls.py
+++ src/lojaAdmin/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loja.urls.HomeUrls')),
     path('produto/', include('loja.urls.ProdutoUrls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
