9b283f536d7315c87ef8163098f2014dd1f92e9c
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-09-05 22:53:57 -0300

    Continuando a estilização da home

--- src/loja/models/Produto.py
+++ src/loja/models/Produto.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.hashers import make_password
-
 from loja.models import *
 import datetime
 
@@ -10,6 +8,8 @@ class Produto(models.Model):
     Dt_fabricacao = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Dt_validade = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Imagem = models.ImageField(null=True, blank=True, upload_to='produtos/')
+    Promocao = models.IntegerField(null=True, default=0)
+    Favorito = models.BooleanField(null=True, default=False)
 
     def __str__(self):
         return f'{self.Nome}'
