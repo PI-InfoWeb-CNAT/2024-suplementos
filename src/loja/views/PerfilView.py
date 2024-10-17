4c865faab14ae9c0949594882374bf493450ae08
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2024-10-17 17:19:09 -0300

    Continuação da página de perfil

--- src/loja/views/PerfilView.py
+++ src/loja/views/PerfilView.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.urls import reverse
+from loja.models import Cliente
 
 def perfil_view(request):
-    if request.user.is_authenticated:
-        return render(request, template_name='perfil.html', status=200)
+    user = request.user
+    clientes = Cliente.objects.all()
+
+    if user.is_authenticated:
+        email = user.email
+        tel_celular = ''
+        tel_fixo = ''
+
+        for cliente in clientes:
+            if cliente.id == user.id:
+                tel_celular = cliente.Telefone_celular
+                tel_fixo = cliente.Telefone_fixo
+
+        context = {
+            'email': email,
+            'tel_celular': tel_celular,
+            'tel_fixo': tel_fixo
+        }
+        return render(request, template_name='perfil.html', context=context, status=200)
     else:
         return redirect('login')
     
