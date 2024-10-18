235c18d1aff866a181bbc45134fe8a9e9d1b57e2
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2024-10-18 17:27:45 -0300

    Funcionalidade de excluir a conta e continuação da página Meu Perfil

--- src/loja/urls/PerfilUrls.py
+++ src/loja/urls/PerfilUrls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from loja.views.PerfilView import perfil_view, logout_view
+from loja.views.PerfilView import perfil_view, logout_view, excluir_conta_view
 
 urlpatterns = [
     path('', perfil_view, name='perfil'),
     path("logout/", logout_view, name='logout'),
+    path("excluir/", excluir_conta_view, name='excluir'),
 ]
--- src/loja/views/PerfilView.py
+++ src/loja/views/PerfilView.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import logout
 from django.urls import reverse
 from loja.models import Cliente
@@ -8,19 +9,24 @@ def perfil_view(request):
     clientes = Cliente.objects.all()
 
     if user.is_authenticated:
+        nome = user.username
         email = user.email
         tel_celular = ''
         tel_fixo = ''
+        cpf = ''
 
         for cliente in clientes:
             if cliente.id == user.id:
+                cpf = cliente.CPF
                 tel_celular = cliente.Telefone_celular
                 tel_fixo = cliente.Telefone_fixo
 
         context = {
+            'nome': nome,
             'email': email,
             'tel_celular': tel_celular,
-            'tel_fixo': tel_fixo
+            'tel_fixo': tel_fixo,
+            'cpf': cpf
         }
         return render(request, template_name='perfil.html', context=context, status=200)
     else:
@@ -29,3 +35,14 @@ def perfil_view(request):
 def logout_view(request):
     logout(request)
     return redirect(reverse('home'))
+
+def excluir_conta_view(request):
+    if request.method == 'POST':
+        user = request.user
+
+        user.delete()
+        logout(request)
+        messages.success(request, 'Conta excluída com sucesso!')
+        return redirect(reverse('home'))
+    
+    
