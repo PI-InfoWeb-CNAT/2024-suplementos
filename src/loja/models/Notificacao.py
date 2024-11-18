72ab3eac0db79b5333d320c1acf3a7fed0161a57
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2024-11-18 10:30:19 -0300

    Falha na notificação

--- src/loja/models/Notificacao.py
+++ src/loja/models/Notificacao.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from loja.models import *
 
 class Notificacao(models.Model):
@@ -7,12 +8,35 @@ class Notificacao(models.Model):
         ('mensagem_personalizada', 'Mensagem Personalizada'),
     ]
 
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True, blank=True)
     categoria = models.CharField(max_length=50, choices=CATEGORIAS)
     titulo = models.CharField(null=False, max_length=100)
     texto = models.TextField(null=False)
     data_envio = models.DateTimeField(auto_now_add=True)
     lida = models.BooleanField(default=False)
+    enviar_para_todos = models.BooleanField(default=False)
+
+    def clean(self):
+        if not self.enviar_para_todos and not self.cliente:
+            raise ValidationError("Selecione um cliente ou marque 'Enviar para todos'.")
+        if self.enviar_para_todos and self.cliente:
+            raise ValidationError("Não é permitido selecionar um cliente e marcar 'Enviar para todos'.")
+
+    def save(self, *args, **kwargs):
+        if self.enviar_para_todos:
+            clientes = Cliente.objects.all()
+            for cliente in clientes:
+                Notificacao.objects.create(
+                    cliente=cliente,
+                    categoria=self.categoria,
+                    titulo=self.titulo,
+                    texto=self.texto,
+                    lida=False,
+                )
+        else:
+            super().save(*args, **kwargs)
 
     def __str__(self):
+        if self.enviar_para_todos:
+            return f'{self.titulo} - Enviado para todos os clientes'
         return f'{self.titulo} - {self.cliente}'
