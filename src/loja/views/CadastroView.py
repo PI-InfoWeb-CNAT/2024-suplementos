45a4389f6aafeea27b4da4bd714ec9b56296b2ad
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-11-23 22:00:00 -0300

    Validação do cpf

--- src/loja/views/CadastroView.py
+++ src/loja/views/CadastroView.py
@@ -14,11 +14,18 @@ def cadastro_view(request):
         confirm_senha = request.POST.get('confirm_senha')
 
         if nome and email and senha and cpf and tel_celular and confirm_senha:
+            cpf = ''.join(filter(str.isdigit, cpf))
+            cpf_validado = validar_cpf(cpf)
+            print(cpf_validado)
             if senha == confirm_senha:
                 if User.objects.filter(email=email).exists():
                     messages.error(request, 'E-mail já cadastrado')
                 elif Cliente.objects.filter(CPF=cpf).exists():
                     messages.error(request, 'CPF já cadastrado')
+                elif not cpf_validado:
+                    messages.error(request, 'CPF inválido')
+                elif len(senha) < 8:
+                    messages.error(request, 'A senha deve ter no mínimo 8 caracteres')
                 else:
                     user = User.objects.create_user(username=nome, email=email, password=senha)
                     user.save()
@@ -42,3 +49,20 @@ def cadastro_view(request):
             messages.error(request, 'Preencha todos os campos')
 
     return render(request, template_name='cadastro.html', status=200)
+
+def validar_cpf(cpf):
+    if len(cpf) != 11 or cpf == cpf[0] * 11:
+        return False
+    
+    def calcular_digito(cpf, fator):
+        soma = sum(int(cpf[i]) * (fator - i) for i in range(len(cpf)))
+        resto = soma % 11
+        return 0 if resto < 2 else 11 - resto
+    
+    primeiro_digito = calcular_digito(cpf[:9], 10)
+    segundo_digito = calcular_digito(cpf[:9] + str(primeiro_digito), 11)
+
+    if cpf[-2:] == f'{primeiro_digito}{segundo_digito}':
+        return True
+    else:
+        return False
