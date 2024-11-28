e510568b742d2634e2194abe7796cb8caa5cd0d1
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2024-11-28 15:47:16 -0300

    Validação dos modelos necessários

--- src/loja/models/Produto.py
+++ src/loja/models/Produto.py
@@ -1,15 +1,16 @@
 from loja.models import *
+from loja.validators import numero_nao_negativo, porcentagem
 import datetime
 
 class Produto(models.Model):
     Nome = models.CharField(null=False, max_length=100)
-    Preco = models.FloatField(null=False)
+    Preco = models.FloatField(null=False, validators=[numero_nao_negativo])
     Descricao = models.CharField(null=False, max_length=300)
     Dt_fabricacao = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Dt_validade = models.DateField(null=False, default=datetime.date(2000, 1, 1))
     Imagem = models.ImageField(null=True, blank=True, upload_to='produtos/')
-    Promocao = models.IntegerField(null=True, default=0)
-    Favorito = models.BooleanField(null=True, default=False)
+    Promocao = models.IntegerField(null=True, default=0, validators=[porcentagem])
+    Estoque = models.IntegerField(null=False, default=10, validators=[numero_nao_negativo])
 
     def __str__(self):
         return f'{self.Nome}'
