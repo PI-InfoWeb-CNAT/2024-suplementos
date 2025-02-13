bf86bfe50621743c0fc8f026602b53f2c8c344c0
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-02-13 19:48:08 -0300

    Alteração no modelo de notificação

--- src/loja/admin.py
+++ src/loja/admin.py
@@ -10,13 +10,7 @@ class ProdutoAdmin(admin.ModelAdmin):
     empty_value_display = 'Vazio'
 
 class NotificacaoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'categoria', 'cliente', 'data_envio', 'enviar_para_todos', 'lida')
-    list_filter = ('categoria', 'enviar_para_todos')
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj and obj.enviar_para_todos:
-            return ['cliente']
-        return super().get_readonly_fields(request, obj)
+    list_display = ('titulo', 'categoria', 'cliente', 'data_envio', 'lida')
 
 class CartaoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'nome_titular', 'bandeira', 'tipo')
--- src/loja/models/Notificacao.py
+++ src/loja/models/Notificacao.py
@@ -14,29 +14,6 @@ class Notificacao(models.Model):
     texto = models.TextField(null=False)
     data_envio = models.DateTimeField(auto_now_add=True)
     lida = models.BooleanField(default=False)
-    enviar_para_todos = models.BooleanField(default=False)
-
-    def clean(self):
-        if not self.enviar_para_todos and not self.cliente:
-            raise ValidationError("Selecione um cliente ou marque 'Enviar para todos'.")
-        if self.enviar_para_todos and self.cliente:
-            raise ValidationError("Não é permitido selecionar um cliente e marcar 'Enviar para todos'.")
-
-    def save(self, *args, **kwargs):
-        if self.enviar_para_todos:
-            clientes = Cliente.objects.all()
-            for cliente in clientes:
-                Notificacao.objects.create(
-                    cliente=cliente,
-                    categoria=self.categoria,
-                    titulo=self.titulo,
-                    texto=self.texto,
-                    lida=False,
-                )
-        else:
-            super().save(*args, **kwargs)
 
     def __str__(self):
-        if self.enviar_para_todos:
-            return f'{self.titulo} - Enviado para todos os clientes'
         return f'{self.titulo} - {self.cliente}'
